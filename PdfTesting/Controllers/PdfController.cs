9ed0756aa2a2c06104bd0ccd4b673989d3757ca1
Luka Radovanovic <lukadlm97@gmail.com>
2020-06-19 23:58:01 +0200

    implemented simple title for docs

--- PdfTesting/Controllers/PdfController.cs
+++ PdfTesting/Controllers/PdfController.cs
@@ -363,6 +363,64 @@ namespace PdfTesting.Controllers
             return File(stream, "application/pdf", "watermark.pdf");
         }
 
+        public IActionResult SimpleTitle()
+        {
+            PdfDocument document = new PdfDocument();
+            PdfPage strana = document.AddPage();
+
+           
+            Kompanija k = new Kompanija()
+            {
+                Naziv = "naziv kompanije",
+                Kontakti = new List<Kontakt>
+                {
+                    new Kontakt
+                    {
+                        Sadrzaj="014821931",
+                        VrstaKontakta = new VrstaKontakta
+                        {
+                            NazivVrsteKontakta="fiksni"
+                        }
+                    },
+                    new Kontakt
+                    {
+                        Sadrzaj="mail@kompani",
+                        VrstaKontakta = new VrstaKontakta
+                        {
+                            NazivVrsteKontakta="email"
+                        }
+                    },
+                },
+                Lokacije = new List<Lokacija>
+                {
+                    new Lokacija
+                    {
+                        NazivUlice="kralja Petra I",
+                        Broj=17,
+                        Sprat=1,
+                        Vrata=4,
+                        Grad = new Grad
+                        {
+                            Naziv="Lajkovac",
+                            PostanskiBroj="14224"
+                        }
+                    }
+                }
+            };
+
+            Naslov naslov = new Naslov();
+            naslov.PostaviVelikNaslov("Naslov predloga",strana);
+
+
+            MemoryStream stream = new MemoryStream();
+
+            document.Save(stream, false);
+            stream.Position = 0;
+
+            return File(stream, "application/pdf", "watermark.pdf");
+        }
+
+
         public IActionResult LayoutWithImg()
         {
             PdfDocument document = new PdfDocument();
--- PdfTestingDemo.PredlogZaSaradnjuLibrary/Naslov.cs
+++ PdfTestingDemo.PredlogZaSaradnjuLibrary/Naslov.cs
@@ -1,4 +1,6 @@
 ﻿using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+using PdfSharp.Drawing.Layout;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +11,15 @@ namespace PdfTestingDemo.PredlogZaSaradnjuLibrary
     {
         public void PostaviVelikNaslov(string naslov,PdfPage stranica)
         {
-            //TODO: create logic
+            XGraphics gfx = XGraphics.FromPdfPage(stranica);
+            XFont font = new XFont("Times New Roman", 18, XFontStyle.Regular);
+            XTextFormatter tf = new XTextFormatter(gfx);
+
+            XRect xRect = new XRect(170, 100, 350, 220);
+            tf.Alignment = XParagraphAlignment.Center;
+            tf.DrawString(naslov, font, XBrushes.Black, xRect, XStringFormats.TopLeft);
+
+            gfx.Dispose();
         }
         public void PostaviBoldiranNaslov(string naslov, PdfPage stranica)
         {
