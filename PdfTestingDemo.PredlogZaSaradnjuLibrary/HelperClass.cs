ff6379052485c55c5834f91e85dfe2d39eb543aa
Luka Radovanovic <lukadlm97@gmail.com>
2020-06-21 02:23:11 +0200

    implemented info about company at request

---
+++ PdfTestingDemo.PredlogZaSaradnjuLibrary/HelperClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PdfTestingDemo.PredlogZaSaradnjuLibrary
+{
+    public class HelperClass
+    {
+        static HelperClass _instance;
+
+        private HelperClass()
+        {
+
+        }
+
+        public static HelperClass Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new HelperClass();
+                return _instance;
+            }
+        }
+
+        public string IspisiKontakt(Kontakt kontakt)
+        {
+            return kontakt.Sadrzaj+" ("+kontakt.VrstaKontakta.NazivVrsteKontakta+")";
+        }
+
+        public string LokacijaHelper(IEnumerable<Lokacija> lokacije)
+        {
+            Lokacija trenutnaLokacija = lokacije.LastOrDefault();
+
+            if (trenutnaLokacija == null)
+                return "Nedostupna lokacija za kompaniju.";
+
+            return " grad: " + trenutnaLokacija.Grad.Naziv + " ul. " + trenutnaLokacija.NazivUlice + " br." + trenutnaLokacija.Broj + " sprat:" + trenutnaLokacija.Sprat + " stan:" + trenutnaLokacija.Vrata;
+        }
+    }
+}
--- PdfTestingDemo.PredlogZaSaradnjuLibrary/InformacijeOKompaniji.cs
+++ PdfTestingDemo.PredlogZaSaradnjuLibrary/InformacijeOKompaniji.cs
@@ -1,4 +1,6 @@
-﻿using PdfSharp.Pdf;
+﻿using PdfSharp.Drawing;
+using PdfSharp.Drawing.Layout;
+using PdfSharp.Pdf;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +11,44 @@ namespace PdfTestingDemo.PredlogZaSaradnjuLibrary
     {
         public void PostaviInformacije(Kompanija k,PdfPage stranica)
         {
-            //TODO: create logic
+            XGraphics gfx = XGraphics.FromPdfPage(stranica);
+            XFont font = new XFont("Times New Roman", 18, XFontStyle.Italic);
+            XFont fontBold = new XFont("Times New Roman", 12, XFontStyle.BoldItalic);
+            XTextFormatter tf = new XTextFormatter(gfx);
+
+            XRect xRect = new XRect(40, 410, 350, 220);
+            tf.Alignment = XParagraphAlignment.Center;
+            tf.DrawString("Informacije o kompaniji "+k.Naziv, font, XBrushes.Black, xRect, XStringFormats.TopLeft);
+
+            font = new XFont("Times New Roman", 12, XFontStyle.Regular);
+
+            xRect = new XRect(60, 450, 350, 220);
+            tf.Alignment = XParagraphAlignment.Left;
+            tf.DrawString("Naziv kompanije: ", fontBold, XBrushes.Black, xRect, XStringFormats.TopLeft);
+            xRect = new XRect(175, 450, 350, 220);
+            tf.DrawString(k.Naziv, font, XBrushes.Black, xRect, XStringFormats.TopLeft);
+
+            xRect = new XRect(60, 465, 350, 220);
+            tf.Alignment = XParagraphAlignment.Left;
+            tf.DrawString("Adresa kompanije: ", fontBold, XBrushes.Black, xRect, XStringFormats.TopLeft);
+            xRect = new XRect(175, 465, 350, 220);
+            tf.DrawString(HelperClass.Instance.LokacijaHelper(k.Lokacije), font, XBrushes.Black, xRect, XStringFormats.TopLeft);
+
+            xRect = new XRect(60, 480, 350, 220);
+            tf.Alignment = XParagraphAlignment.Left;
+            tf.DrawString("Kontakti kompanije: ", fontBold, XBrushes.Black, xRect, XStringFormats.TopLeft);
+
+            int variableHigh = 480;
+
+            foreach(Kontakt kontakt in k.Kontakti){
+                xRect = new XRect(175, variableHigh, 350, 220);
+                tf.DrawString(HelperClass.Instance.IspisiKontakt(kontakt), font, XBrushes.Black, xRect, XStringFormats.TopLeft);
+                variableHigh += 15;
+            }
+
+
+
+            gfx.Dispose();
         }
         public void PostaviInformacijeBold(Kompanija k, PdfPage stranica)
         {
